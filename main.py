ad4dc10ac49bfe86091ebdba7c0ea6367b301477
AP <78767681+s5253812@users.noreply.github.com>
2024-05-27 15:42:51 -0700

    add card flipping feature

--- main.py
+++ main.py
@@ -5,12 +5,25 @@ from random import choice
 BACKGROUND_COLOR = "#B1DDC6"
 FONT = "Ariel"
 
+curr_word = {}
+
+
+def flip_card():
+	global curr_word
+	canvas.itemconfig(canvas_image, image=image_back)
+	canvas.itemconfig(title_tag, text="English", fill="white")
+	canvas.itemconfig(word_tag, text=f"{curr_word['English']}", fill="white")
+
 
 def next_card():
+	global curr_word, flip_timer
+	window.after_cancel(flip_timer)
 	curr_word = choice(data)
 	word_fr = curr_word["French"]
-	canvas.itemconfig(title_tag, text="French")
-	canvas.itemconfig(word_tag, text=f"{word_fr}")
+	canvas.itemconfig(canvas_image, image=image_front)
+	canvas.itemconfig(title_tag, text="French", fill="black")
+	canvas.itemconfig(word_tag, text=f"{word_fr}", fill="black")
+	flip_timer = window.after(3000, func=flip_card)
 
 
 # Get words from csv data
@@ -20,7 +33,7 @@ data = pandas.read_csv("data/french_words.csv").to_dict(orient="records")
 window = Tk()
 window.title("Flash Cards")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
-
+flip_timer = window.after(3000, flip_card)
 # GUI
 image_right = PhotoImage(file="images/right.png")
 image_wrong = PhotoImage(file="images/wrong.png")
@@ -28,7 +41,7 @@ image_front = PhotoImage(file="images/card_front.png")
 image_back = PhotoImage(file="images/card_back.png")
 
 canvas = Canvas(width=800, height=526)
-canvas.create_image(400, 263, image=image_front)
+canvas_image = canvas.create_image(400, 263, image=image_front)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
