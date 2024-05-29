0df6b4ef3e1afa1ee23f301b3c9066e74eddea31
aerinp <s5253812@gmail.com>
2024-05-28 18:47:55 -0700

    add save progress feature

--- main.py
+++ main.py
@@ -8,26 +8,35 @@ FONT = "Ariel"
 curr_word = {}
 
 
+# Flips the card
 def flip_card():
-	global curr_word
-	canvas.itemconfig(canvas_image, image=image_back)
-	canvas.itemconfig(title_tag, text="English", fill="white")
-	canvas.itemconfig(word_tag, text=f"{curr_word['English']}", fill="white")
+    global curr_word
+    canvas.itemconfig(canvas_image, image=image_back)
+    canvas.itemconfig(title_tag, text="English", fill="white")
+    canvas.itemconfig(word_tag, text=f"{curr_word['English']}", fill="white")
 
 
+# Move on to the next word in the list
 def next_card():
-	global curr_word, flip_timer
-	window.after_cancel(flip_timer)
-	curr_word = choice(data)
-	word_fr = curr_word["French"]
-	canvas.itemconfig(canvas_image, image=image_front)
-	canvas.itemconfig(title_tag, text="French", fill="black")
-	canvas.itemconfig(word_tag, text=f"{word_fr}", fill="black")
-	flip_timer = window.after(3000, func=flip_card)
+    global curr_word, flip_timer
+    window.after_cancel(flip_timer)
+    curr_word = choice(data)
+    word_fr = curr_word["French"]
+    canvas.itemconfig(canvas_image, image=image_front)
+    canvas.itemconfig(title_tag, text="French", fill="black")
+    canvas.itemconfig(word_tag, text=f"{word_fr}", fill="black")
+    flip_timer = window.after(3000, func=flip_card)
 
 
-# Get words from csv data
-data = pandas.read_csv("data/french_words.csv").to_dict(orient="records")
+# Remove the curr_word from the list of words to learn
+def remove_word():
+    data.remove(curr_word)
+
+# Save all the words in the data into data/words_to_learn.csv
+def save_words():
+    new_words_list = pandas.DataFrame(data)
+    new_words_list.to_csv("data/words_to_learn.csv", index=False)
+
 
 # Screen
 window = Tk()
@@ -48,14 +57,21 @@ canvas.grid(row=0, column=0, columnspan=2)
 title_tag = canvas.create_text(400, 150, text="French", font=(FONT, 40, "italic"))
 word_tag = canvas.create_text(400, 275, text="Word", font=(FONT, 60, "bold"))
 
-button_wrong = Button(image=image_wrong, command=next_card)
+button_wrong = Button(image=image_wrong, command=lambda: [save_words(), next_card()])
 button_wrong.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 button_wrong.grid(row=1, column=0)
 
-button_correct = Button(image=image_right, command=next_card)
+button_correct = Button(image=image_right, command=lambda: [remove_word(), save_words(), next_card()])
 button_correct.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 button_correct.grid(row=1, column=1)
 
-next_card()
+# Get the words from data/words_to_learn.csv if exists
+# Or else, get the words from data/french_words.csv
+try:
+    data = pandas.read_csv("data/words_to_learn.csv").to_dict(orient="records")
+except FileNotFoundError or pandas.errors.EmptyDataError:
+    data = pandas.read_csv("data/french_words.csv").to_dict(orient="records")
+finally:
+    next_card()
 
 window.mainloop()
